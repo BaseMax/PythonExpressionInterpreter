ad0db94205ee041ad9fb1d5cb9746c8734300f72
Max Base <MaxBaseCode@Gmail.Com>
2021-05-23 04:33:35 +0430

    use Interpreter in main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from lexer import Lexer
 from parser import Parser
+from interpreter import Interpreter
 
 while True:
-	text = input("calc > ")
-	lexer = Lexer(text)
-	tokens = lexer.generate_tokens()
-	print(list(tokens))
-	parser = Parser(tokens)
-	tree = parser.parse()
-	print(tree)
+	try:
+		text = input("calc > ")
+		lexer = Lexer(text)
+		tokens = lexer.generate_tokens()
+		print(list(tokens))
+		parser = Parser(tokens)
+		tree = parser.parse()
+		print(tree)
+		if not tree: continue
+		interpreter = interpreter()
+		value = interpreter.visit(tree)
+		print(value)
+	except Exception as e:
+		print(e)
