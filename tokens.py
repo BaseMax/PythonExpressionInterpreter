83ce24eac3e35da49fbb16250d05be41ed12f594
Max Base <MaxBaseCode@Gmail.Com>
2021-05-23 04:49:32 +0430

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -2,13 +2,13 @@ from enum import Enum
 from dataclasses import dataclass
 
 class TokenType(Enum):
-	NUMBER = 0
-	PLUS = 1
-	MINUS = 2
-	MULTIPLY = 3
-	DIVIDE = 4
-	LPARENT = 5
-	RPARENT = 6
+	NUMBER    = 0
+	PLUS      = 1
+	MINUS     = 2
+	MULTIPLY  = 3
+	DIVIDE    = 4
+	LPAREN    = 5
+	RPAREN    = 6
 
 @dataclass
 class Token:
@@ -17,4 +17,3 @@ class Token:
 
 	def __repr__(self):
 		return self.type.name + (f":{self.value}" if self.value != None else "")
-
