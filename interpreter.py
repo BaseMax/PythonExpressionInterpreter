296b8caaa786264eb5b9214c34bfafcc0536760a
Max Base <MaxBaseCode@Gmail.Com>
2021-05-23 04:50:00 +0430

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -2,18 +2,21 @@ from nodes import *
 from values import Number
 
 class Interpreter:
+	def __init__(self):
+		pass
+
 	def visit(self, node):
 		method_name = f'visit_{type(node).__name__}'
 		method = getattr(self, method_name)
 		return method(node)
-
+		
 	def visit_NumberNode(self, node):
 		return Number(node.value)
 
 	def visit_AddNode(self, node):
 		return Number(self.visit(node.node_a).value + self.visit(node.node_b).value)
 
-	def visit_MinusNode(self, node):
+	def visit_SubtractNode(self, node):
 		return Number(self.visit(node.node_a).value - self.visit(node.node_b).value)
 
 	def visit_MultiplyNode(self, node):
