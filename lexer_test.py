576c50dc9fe0ce31d88e9cc32b685cebf4312388
Max Base <MaxBaseCode@Gmail.Com>
2021-05-23 04:48:38 +0430

    Update lexer_test.py

--- lexer_test.py
+++ lexer_test.py
@@ -1,60 +1,56 @@
 import unittest
+
 from tokens import Token, TokenType
-from lexer import L exer
+from lexer import Lexer
 
 class TestLexer(unittest.TestCase):
-
+	
 	def test_empty(self):
 		tokens = list(Lexer("").generate_tokens())
 		self.assertEqual(tokens, [])
-
-	def test_empty(self):
-		tokens = list(Lexer("  \t\n  \t\t\t\t\n\n").generate_tokens())
+	
+	def test_whitespace(self):
+		tokens = list(Lexer(" \t\n  \t\t\n\n").generate_tokens())
 		self.assertEqual(tokens, [])
 
-
-	def test_number(self):
+	def test_numbers(self):
 		tokens = list(Lexer("123 123.456 123. .456 .").generate_tokens())
 		self.assertEqual(tokens, [
 			Token(TokenType.NUMBER, 123.000),
 			Token(TokenType.NUMBER, 123.456),
 			Token(TokenType.NUMBER, 123.000),
 			Token(TokenType.NUMBER, 000.456),
-			Token(TokenType.NUMBER, 000.000)
+			Token(TokenType.NUMBER, 000.000),
 		])
-
+	
 	def test_operators(self):
 		tokens = list(Lexer("+-*/").generate_tokens())
 		self.assertEqual(tokens, [
 			Token(TokenType.PLUS),
 			Token(TokenType.MINUS),
 			Token(TokenType.MULTIPLY),
-			Token(TokenType.DIVIDE)
+			Token(TokenType.DIVIDE),
 		])
 
 	def test_parens(self):
 		tokens = list(Lexer("()").generate_tokens())
 		self.assertEqual(tokens, [
-			Token(TokenType.LPARENT),
-			Token(TokenType.RPARENT)
+			Token(TokenType.LPAREN),
+			Token(TokenType.RPAREN),
 		])
-
+	
 	def test_all(self):
 		tokens = list(Lexer("27 + (43 / 36 - 48) * 51").generate_tokens())
 		self.assertEqual(tokens, [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.PLUS),
-
-			Token(TokenType.LPARENT),
+			Token(TokenType.LPAREN),
 			Token(TokenType.NUMBER, 43),
 			Token(TokenType.DIVIDE),
 			Token(TokenType.NUMBER, 36),
 			Token(TokenType.MINUS),
 			Token(TokenType.NUMBER, 48),
-			Token(TokenType.MILTIPLY),
-			Token(TokenType.RPARENT),
-
-			Token(TokenType.NUMBER, 51)
+			Token(TokenType.RPAREN),
+			Token(TokenType.MULTIPLY),
+			Token(TokenType.NUMBER, 51),
 		])
-
-
