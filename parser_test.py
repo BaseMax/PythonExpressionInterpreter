b10e8a921831a75645c5452dd08f4dfb2c3ad5b1
Max Base <MaxBaseCode@Gmail.Com>
2021-05-23 04:48:24 +0430

    Update parser_test.py

--- parser_test.py
+++ parser_test.py
@@ -1,6 +1,7 @@
 import unittest
+
 from tokens import Token, TokenType
-from parser import Parser
+from parser_ import Parser
 from nodes import *
 
 class TestParser(unittest.TestCase):
@@ -15,40 +16,40 @@ class TestParser(unittest.TestCase):
 		node = Parser(tokens).parse()
 		self.assertEqual(node, NumberNode(51.2))
 
-	def test_individual_operations(self):
+	def test_single_operations(self):
 		tokens = [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.PLUS),
 			Token(TokenType.NUMBER, 14),
 		]
+
 		node = Parser(tokens).parse()
 		self.assertEqual(node, AddNode(NumberNode(27), NumberNode(14)))
-
-
+		
 		tokens = [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.MINUS),
 			Token(TokenType.NUMBER, 14),
 		]
+
 		node = Parser(tokens).parse()
 		self.assertEqual(node, SubtractNode(NumberNode(27), NumberNode(14)))
-
-
+			
 		tokens = [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.MULTIPLY),
 			Token(TokenType.NUMBER, 14),
 		]
+
 		node = Parser(tokens).parse()
 		self.assertEqual(node, MultiplyNode(NumberNode(27), NumberNode(14)))
-
-
-
+			
 		tokens = [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.DIVIDE),
 			Token(TokenType.NUMBER, 14),
 		]
+
 		node = Parser(tokens).parse()
 		self.assertEqual(node, DivideNode(NumberNode(27), NumberNode(14)))
 
@@ -56,23 +57,23 @@ class TestParser(unittest.TestCase):
 		tokens = [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.PLUS),
-
-			Token(TokenType.LPARENT),
+			Token(TokenType.LPAREN),
 			Token(TokenType.NUMBER, 43),
 			Token(TokenType.DIVIDE),
 			Token(TokenType.NUMBER, 36),
 			Token(TokenType.MINUS),
 			Token(TokenType.NUMBER, 48),
-			Token(TokenType.MILTIPLY),
-			Token(TokenType.RPARENT),
-
-			Token(TokenType.NUMBER, 51)
+			Token(TokenType.RPAREN),
+			Token(TokenType.MULTIPLY),
+			Token(TokenType.NUMBER, 51),
 		]
+
 		node = Parser(tokens).parse()
 		self.assertEqual(node, AddNode(
 			NumberNode(27),
-				MultiplyNode(
-					SubtractNode(
+			MultiplyNode(
+				SubtractNode(
+					DivideNode(
 						NumberNode(43),
 						NumberNode(36)
 					),
@@ -80,4 +81,4 @@ class TestParser(unittest.TestCase):
 				),
 				NumberNode(51)
 			)
-		)
+		))
